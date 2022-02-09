ab376bcbf3ed8c853452dd3f5de87a34d3e98e8a
youpy <youpy@buycheapviagraonlinenow.com>
2022-02-10 01:13:52 +0900

    s/path/route/g

--- src/route.test.ts
+++ src/route.test.ts
@@ -1,15 +1,15 @@
 import { route } from './route'
 
-describe('path', () => {
-  it('parses a path', () => {
-    const p = route('/foo/bar/baz')
+describe('route', () => {
+  it('parses a route', () => {
+    const r = route('/foo/bar/baz')
 
-    expect(p()).toEqual('/foo/bar/baz')
+    expect(r()).toEqual('/foo/bar/baz')
   })
 
-  it('parses a path with parameters', () => {
-    const p = route('/foo/:param1/bar/:param2')
+  it('parses a route with parameters', () => {
+    const r = route('/foo/:param1/bar/:param2')
 
-    expect(p({ param1: 'xxx', param2: 'yyy' })).toEqual('/foo/xxx/bar/yyy')
+    expect(r({ param1: 'xxx', param2: 'yyy' })).toEqual('/foo/xxx/bar/yyy')
   })
 })
